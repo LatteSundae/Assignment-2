7c45646a502fcf70ee175dd9a98624f7cae93f13
Prasong Chonsathian <prasongc1995@gmail.com>
2021-02-23 22:28:43 +0700

    DB Connection

--- web/config.php
+++ web/config.php
@@ -1,6 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "0854850808";
-$dbname = "employees";
+//$servername = "localhost";
+//$username = "root";
+//$password = "0854850808";
+//$dbname = "employees";
+
+$url = parse_url(getenv("DATABASE_URL"));
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbname = substr($url["path"], 1);
+echo "Extracted Info $servername $username $password $dbnane<br>";
 ?>
+
